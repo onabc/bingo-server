0ff051b1e75392d37958cd8ba5341add2dd51110
DaboQ <hesbiu@gmail.com>
2021-12-13 17:53:28 +0800

    feat(websocket):删除冗余的注解

--- bingo-websocket/src/main/java/com/bingo/websocket/WebSocketApplication.java
+++ bingo-websocket/src/main/java/com/bingo/websocket/WebSocketApplication.java
@@ -2,16 +2,14 @@ package com.bingo.websocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author lumen
  * @version 1.0
- * @description: TODO
+ * @description: websocket服务
  * @date 2021/12/13 15:23
  */
 @SpringBootApplication
-@ComponentScan(value = {"com.bingo.websocket"})
 public class WebSocketApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebSocketApplication.class, args);
